563c739d826d6c7a3fbe081696c06be084c36932
ajinkyashivpure <ajinkyashivpure@gmail.com>
2025-08-02 14:39:35 +0530

    initialising projet, checking basics

---
+++ src/test/java/com/undo/coursesearch/CourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undo.coursesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
